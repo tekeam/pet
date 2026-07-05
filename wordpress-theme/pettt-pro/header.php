9ae5c9d5389253bcbdb1e49d7a74e7d499788ae1
tekeam <143910393+tekeam@users.noreply.github.com>
2026-07-06 00:15:38 +0330

    Upgrade header with mega menu

--- wordpress-theme/pettt-pro/header.php
+++ wordpress-theme/pettt-pro/header.php
@@ -17,10 +17,15 @@
       <?php wp_nav_menu(['theme_location'=>'primary','container'=>false,'fallback_cb'=>false]); ?>
     </nav>
     <div class="pettt-user-menu">
-      <a href="<?php echo esc_url(home_url('/my-account')); ?>" class="pettt-icon-btn">حساب کاربری</a>
+      <a href="<?php echo esc_url(home_url('/pettt-account')); ?>" class="pettt-icon-btn">پروفایل پت</a>
       <?php if (class_exists('WooCommerce')): ?>
         <a href="<?php echo esc_url(wc_get_cart_url()); ?>" class="pettt-cart-btn">سبد خرید</a>
       <?php endif; ?>
+    </div>
+    <div class="pettt-mega">
+      <div class="pettt-mega-card"><h3>کاتالوگ غذا</h3><a href="<?php echo esc_url(get_post_type_archive_link('pettt_brand')); ?>">برندها</a><a href="<?php echo esc_url(get_post_type_archive_link('pettt_food')); ?>">مدل‌های غذا</a><a href="<?php echo esc_url(home_url('/shop')); ?>">فروشگاه</a></div>
+      <div class="pettt-mega-card"><h3>خدمات و آموزش</h3><a href="<?php echo esc_url(get_post_type_archive_link('pettt_service')); ?>">خدمات شهری</a><a href="<?php echo esc_url(get_post_type_archive_link('pettt_video')); ?>">ویدیوها</a><a href="<?php echo esc_url(home_url('/blog')); ?>">مقالات</a></div>
+      <div class="pettt-mega-card"><h3>اجتماع Pettt</h3><a href="<?php echo esc_url(get_post_type_archive_link('pettt_explore')); ?>">اکسپلور</a><a href="<?php echo esc_url(home_url('/pettt-account')); ?>">پروفایل من</a></div>
     </div>
   </div>
 </header>
